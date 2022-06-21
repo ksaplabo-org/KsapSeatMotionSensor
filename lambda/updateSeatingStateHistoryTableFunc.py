48c64b1f1f10b4452a6ef1a177ab43c9ecdd9534
Ryutanaka <tanakatennis57@gmail.com>
2022-06-21 14:23:09 +0900

    commit

--- lambda/updateSeatingStateHistoryTableFunc.py
+++ lambda/updateSeatingStateHistoryTableFunc.py
@@ -9,41 +9,56 @@ def lambda_handler(event, context):
     for record in event['Records']:
         
         try:
-            print(record['dynamodb'])
             state_data = record['dynamodb']['NewImage']['State']['S']
             mac_address = record['dynamodb']['NewImage']['EspMacAddress']['S']
         except Exception as e:
+            print(1)
             print(e)
             continue
     
-        print(mac_address)
+        try:        
+            #マスタDB接続処理
+            master_dynamodb = boto3.resource('dynamodb')
+            master_table = master_dynamodb.Table('ksap-seatingmaster-tbl')
         
-        #DynamoDB接続処理
-        master_dynamodb = boto3.resource('dynamodb')
-        master_table = master_dynamodb.Table('ksap-seatingmaster-tbl')
-    
-        #マスターテーブルのMACアドレスから名前を取得
-        res = master_table.get_item(
-            Key={
-                'EspMacAddress': mac_address
-            }
-        )
-    
-        item = res['Item']
-        print(item)
+            #マスターテーブルのMACアドレスから名前を取得
+            res = master_table.get_item(
+                Key={
+                    'EspMacAddress': mac_address
+                }
+            )
         
-        #DynamoDB接続処理
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table('ksap-seatingstate-tbl')
-        
-        #SeatNameをキーに値を状態を更新
-        ret = table.put_item(
-            Item={
-                'SeatName': item['SeatName'],
-                'SeatNameJP':item['SeatNameJP'],
-                'State':state_data
-            }
-        )
+            item = res['Item']
+            
+            #DynamoDB接続処理
+            dynamodb = boto3.resource('dynamodb')
+            table = dynamodb.Table('ksap-seatingstate-tbl')
+            
+            #状態がテレワークの場合、状態の更新はしない
+            res2 = table.get_item(
+                Key={
+                    'SeatName':item['SeatName']
+                }
+            )
+            
+            #状態がテレワーク
+            if res2['Item']['State'] == "Telework":
+                #何もしない
+                pass
+            else:
+                #SeatNameをキーに値を状態を更新
+                ret = table.put_item(
+                    Item={
+                        'SeatName': item['SeatName'],
+                        'SeatNameJP':item['SeatNameJP'],
+                        'State':state_data
+                    }
+                )
+        except Exception as e:
+            print(e)
+            print(2)
+            continue
+            
     return{
         'statusCode':200,
         'body': "Success!!!"
